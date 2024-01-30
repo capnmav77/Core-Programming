fe3808cf207cd0e0f822067a4796148bae1ef4a8
Meowmaster <ashenoy64@gmail.com>
2024-01-30 12:41:35 +0530

    Ramesh wants this code itseems

--- Generic Prog/Unit-01/Avanish/implicit_template_inst.cpp
+++ Generic Prog/Unit-01/Avanish/implicit_template_inst.cpp
@@ -2,18 +2,43 @@
 
 using namespace std;
 
+// template <typename T>
+// struct foo{
+//     void f(){}
+//     void g(){
+//         int x="200";
+//     }
+// };
+
+// int main()
+// {
+//     foo<int>* x;
+//     foo<int> y;
+//     foo<double>* q;
+//     q->f();
+// }
+
 template <typename T>
 struct foo{
-    void f(){}
-    void g(){
-        int x="200";
-    }
+    static T data;
 };
+
+
+template<typename T>T foo<T>::data = 0;
 
 int main()
 {
-    foo<int>* x;
-    foo<int> y;
-    foo<double>* q;
-    q->f();
+    foo<int> a;
+    foo<double> b;
+    foo<double> c;
+
+    cout<<a.data<<endl;
+    cout<<b.data<<endl;
+    cout<<c.data<<endl;
+
+    b.data = 42;
+
+    cout<<a.data<<endl;
+    cout<<b.data<<endl;
+    cout<<c.data<<endl;
 }
