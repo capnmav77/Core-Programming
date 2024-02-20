9a660dcd4dea6c5ac4ab3fbbd92505ca3570f1f0
capnmav77 <its.mastermind77@gmail.com>
2024-02-20 07:04:51 +0530

    Revision notes half of unit one

--- Generic Prog/Unit-01/Anand Ms slides and Programs/All Programs/ConstExpr.cpp
+++ Generic Prog/Unit-01/Anand Ms slides and Programs/All Programs/ConstExpr.cpp
@@ -8,9 +8,16 @@ int main()
 {
 	const int dmv = 17; // dmv is a named constant
     int var = 17; // var is not a constant
-    constexpr double max1 = 1.4 * sqrt(dmv); // OK if square(17) is a constant expression
-//    constexpr double max2 = 1.4 * sqrt(var); // error : var is not a constant expression
+    constexpr double max1 = 1.4 * sqrt(dmv); // OK if square(17) is a constant expression because dmv is a named constant
+   // constexpr double max2 = 1.4 * sqrt(var); // error : var is not a constant expression because it is not a named constant
     const double max3 = 1.4 * sqrt(var); // OK, may be evaluated at run time
+    // the difference between const and constexpr is that constexpr must be evaluated at compile time and const can be evaluated at run time
+    // constexpr is used to define constants that must be evaluated at compile time
+    // const is used to define constants that can be evaluated at run time
+    cout << max1 << endl;
+   // cout << max2 << endl;
+    cout << max3 << endl;
+
 
 	return 0;
 }
