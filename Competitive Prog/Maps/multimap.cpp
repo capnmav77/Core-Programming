ad86b109c565c3c595905decb423cc1d0fa7a698
capnmav77 <its.mastermind77@gmail.com>
2024-03-12 12:24:30 +0530

    changes in dsa

---
+++ Competitive Prog/Maps/multimap.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+
+    // declaration of multimap 
+    // multimap <Dtype , Dtype> variable_name;
+
+    multimap<int, string> m;
+
+    // insertion into the multimap
+    m.insert({1, "abc"});
+    m.insert({2, "def"});
+    m.insert({2, "ghi"});
+
+    // accessing the multimap
+    // multimap iterator
+    for(auto it = m.begin(); it != m.end(); it++)
+    {
+        cout<<it->first<<" "<<it->second<<endl;
+    }
+
+    
+    return 0;
+}
