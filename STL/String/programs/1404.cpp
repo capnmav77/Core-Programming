23612cb482040f4c7d6509a46dc63bc3094c2e45
capnmav77 <its.mastermind77@gmail.com>
2024-05-29 21:30:21 +0530

    daily leetcode

---
+++ STL/String/programs/1404.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int numSteps(string s) {
+        int l = s.length() - 1;
+        int carry = 0;
+        int count = 0;
+        
+        while (l > 0) {
+            // even number with carry = 0, result even
+            if (s[l] - '0' + carry == 0) {
+                carry = 0;
+                count++;
+            // odd number with carry = 1, result even
+            } else if (s[l] - '0' + carry == 2) {
+                carry = 1;
+                count++;
+            // even number with carry = 1, result odd
+            // odd number with carry = 0, result odd
+            } else {
+                carry = 1;
+                count += 2;
+            }
+            l--;
+        }
+        
+        // last digit 1 needs to add a carried over digit 1, which gives 10.
+        // So need one more divide to make it 1 (one more step)
+        if (carry == 1) {
+            count++;
+        }
+        
+        return count;
+    }
+};
+
+int main()
+{
+    Solution s;
+    if(s.numSteps("1101") == 6)
+        cout << "Test case 1 passed" << endl;
+    else
+        cout << "Test case 1 failed" << endl;
+
+    return 0;
+}
